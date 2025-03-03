33045438a846ca523b2e1b1984b950392f627f6f
Yiwen Wang <wangyiwenau@gmail.com>
2025-03-03 16:36:04 +1100

    use multer to handle the post request from form data

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 const express = require("express");
 const feedRoutes = require("./routes/feed");
 const bodyParser = require("body-parser");
+const multer = require("multer");
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images"); // Save uploaded files to 'images/' directory
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      new Date().toISOString().replace(/:/g, "-") + "-" + file.originalname
+    );
+  },
+});
+
+// File filter (optional, allows only certain file types)
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/jpg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*"); //allow access from any client to avoid cors error
--- controllers/feed.js
+++ controllers/feed.js
@@ -5,15 +5,23 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
-  console.log(req.body);
+  if (!req.file) {
+    const error = new Error("No file uploaded!");
+    error.statusCode(422);
+    throw error;
+  }
+
   const title = req.body.title;
   const content = req.body.content;
+  const imageUrl = req.file.path; // Get the uploaded image file path
+
   res.status(201).json({
     message: "Post created successfully!",
     post: {
       id: new Date().toISOString(),
       title: title,
       content: content,
+      imageUrl: imageUrl,
     },
   });
 };
