9515aadfdfbaad40fc9eddc86b0af7dc2dd09d4d
Yiwen Wang <wangyiwenau@gmail.com>
2025-03-11 17:15:33 +1100

    create user model

---
+++ models/user.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+});
+
+export default mongoose.model("User", userSchema);
