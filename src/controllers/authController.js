7b5e46a5ca1ca7a8b3522dd33ac1a5f69c24a424
Yiwen Wang <wangyiwenau@gmail.com>
2025-03-12 17:09:35 +1100

    add more validation for user signup

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,7 +4,6 @@ import User from "../models/user.js";
 import { deleteFile } from "../utils/aws-s3.js";
 
 export const signup = async (req, res, next) => {
-  console.log(req.body);
   try {
     // in case of validate failed
     const errors = validationResult(req);
@@ -56,4 +55,56 @@ export const signup = async (req, res, next) => {
   }
 };
 
-export default { signup };
+export const login = async (req, res, next) => {
+  try {
+    // in case of validate failed
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // Delete the file from S3 if validation fails.
+      if (req.file && req.file?.key) {
+        await deleteFile(req.file.key);
+      }
+      // Get error message from validator
+      const errorMessages = errors
+        .array()
+        .map((err) => err.msg)
+        .join(" ");
+
+      const error = new Error(
+        errorMessages || "Validation failed, entered data is incorrect."
+      );
+      error.statusCode = 422;
+      return next(error);
+    }
+
+    // create post object from req
+    const email = req.body.email;
+    const password = req.body.password;
+    // Hash the password with bcrypt
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const name = req.body.name;
+    const key = req.file?.key; // key is the file name
+    const thumbNailUrl = req.file?.location; // Get the uploaded image url from s3
+
+    const user = new User({
+      email: email,
+      password: hashedPassword,
+      name: name,
+      key: key,
+      thumbnailUrl: thumbNailUrl,
+      status: "offline",
+    });
+    // insert object into mongodb
+    const result = await user.save();
+    res.status(201).json({
+      message: "User created successfully!",
+      user: result,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export default { signup, login };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,13 +2,29 @@ import express from "express";
 import { body } from "express-validator";
 import authController from "../controllers/authController.js";
 import upload from "../middlewares/multer-config.js";
+import User from "../models/user.js";
 
-const validateUser = [
-  body("email").isEmail().withMessage("Please enter a valid email."),
+const validateSignup = [
+  body("email")
+    .isEmail()
+    .withMessage("Please enter a valid email.")
+    .custom(async (value) => {
+      const existingUser = await User.findOne({ email: value });
+      if (existingUser) {
+        return Promise.reject("Email already been used.");
+      }
+      return true;
+    })
+    .normalizeEmail(),
   body("password")
     .trim()
     .isLength({ min: 5 })
     .withMessage("Please enter a valid password."),
+  body("name")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("Please enter a valid user name."),
 ];
 
 const router = express.Router();
@@ -16,7 +32,7 @@ const router = express.Router();
 router.post(
   "/signup",
   upload.single("image"),
-  validateUser,
+  validateSignup,
   authController.signup
 );
 
